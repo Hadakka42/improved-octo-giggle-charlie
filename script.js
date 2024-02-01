81623fc2af64170c508d2f2a84def6a03d65a61c
Hadakka42 <156353289+Hadakka42@users.noreply.github.com>
2024-02-01 11:40:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  //alert("Top: " + $("img").position().top + "Left: " + $("img").position().left)
+  
+    $("img").draggable();
+//      let rayLeft =$("img").position().left
+//      if (rayLeft > 500) {
+//        console.log("We are above 500");
+//     }  else {
+//        console.log("Not yet above 500");
+//     }
+//                      });
+//   //todo:if position is on right side of page, tell user they won
+//    let rayLeft = $("img").position().left
+//    if (rayLeft > 500) {
+//      console.log("We are above 500");
+//    } else {
+//      console.log("Not yet above 500");
+//    }
+  
+});
